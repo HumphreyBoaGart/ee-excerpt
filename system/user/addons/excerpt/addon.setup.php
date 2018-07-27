fdfed42c614520a6b37f14dd26fbcb8b5ed3dde6
Dennis Wyman <capncornflake@users.noreply.github.com>
2018-07-27 00:19:53 -0500

    cleanup

--- system/user/addons/excerpt/addon.setup.php
+++ system/user/addons/excerpt/addon.setup.php
@@ -1,10 +1,11 @@
 <?php
 
 return array(
-    'author'      => 'Dennis Wyman',
-    'author_url'  => 'https://github.com/capncornflake/ExpressionEngine-Excerpt-Plugin',
     'name'        => 'Excerpt',
-    'description' => 'Limits the number of words in some text, after stripping tags. Based on thinkclay\'s obscenely useful EE2 plugin.',
+    'docs_url'    => 'https://github.com/capncornflake/ExpressionEngine-Excerpt-Plugin',
+    'author'      => 'Dennis Wyman',
+    'author_url'  => 'https://denniswyman.com',
     'version'     => '2.0',
     'namespace'   => 'Excerpt\Excerpt',
+    'description' => 'Limits the number of words in some text, after stripping tags. Based on thinkclay\'s obscenely useful EE2 plugin.',
 );
